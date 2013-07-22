16fa8c8a27212fcd4b9789dc642c1624dad9e9c8
Baker Wang <baikangwang@hotmail.com>
2013-07-22 10:31:20 +0800

    Initial check in

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace dotTraceUtility
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtName.Location = new System.Drawing.Point(12, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(192, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKey.Location = new System.Drawing.Point(12, 38);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.ReadOnly = true;
+            this.txtKey.Size = new System.Drawing.Size(239, 20);
+            this.txtKey.TabIndex = 1;
+            // 
+            // btStart
+            // 
+            this.btStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btStart.Location = new System.Drawing.Point(210, 12);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(41, 20);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 68);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtName);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dotTraceUtility";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button btStart;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+//using BigInteger = System.Numerics.BigInteger;
+
+namespace dotTraceUtility
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            txtName.Text = Environment.MachineName;
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            BigInteger n = Parse("5852679291121824840551871814155310876070738979171116374633");
+            BigInteger p = Parse("76968069673302466210259040287");
+            BigInteger q = Parse("76040354343872999195552087159");
+
+            BigInteger z = (p - 1) * (q - 1);
+
+            BigInteger u = new BigInteger(Encoding.Default.GetBytes(txtName.Text));
+            u |= 1;
+
+            BigInteger d = u.modInverse(z);
+
+            int iUserHash = UserHash(txtName.Text, String.Empty);
+
+            // 时间
+            BigInteger m = new BigInteger((long)65535);
+            m <<= 0x38;
+            m += new BigInteger((long)iUserHash); // 用户名和公司的校验
+            // CustomerId
+            m += new BigInteger((Int32)1) << 0x58;
+            //// Type
+            //m += new BigInteger((Int16)1) << 0x20;
+            // Version
+            m += new BigInteger((Int32)0xFA0) << 0x10;
+            // ProductVersion
+            m += new BigInteger((Int16)0xFA0) << 0x48;
+            //// Edition
+            //m += new BigInteger((Int32)1) << 0x78;
+
+            BigInteger lic = m.modPow(d, n);
+            txtKey.Text = Convert.ToBase64String(lic.getBytes());
+        }
+
+        BigInteger Parse(String str)
+        {
+            return new BigInteger(str, 10);
+        }
+
+        // UserHash ，直接从反编译的源代码中抄过来的 
+        // 作用是判断用户名与License是否匹配 
+        private int UserHash(String username, String company)
+        {
+            int i = 0;
+            for (int j = 0; j < username.Length; j++)
+            {
+                i = ((i << 7) + username[j]) % 65521;
+            }
+
+            for (int k = 0; k < company.Length; k++)
+            {
+                i = ((i << 7) + company[k]) % 65521;
+            }
+            return (i);
+        }
+    }
+}
